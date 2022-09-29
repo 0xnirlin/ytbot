994ffa770493a734b04a94c9a00bfa36ed868484
AhmadDecoded <ahmadhameedkhan80@gmail.com>
2022-09-30 01:21:14 +0500

    Initial commit

---
+++ downloader.py
@@ -0,0 +1,39 @@
+
+import string
+from fastapi import FastAPI,Request
+import random
+import string
+from pydantic import BaseModel
+
+
+
+# importing the module
+from pytube import YouTube
+
+# where to save
+SAVE_PATH = "D:/"  # to_do
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/download")
+async def downloadFile(request:Request):
+    body =  await request.json()
+    print(body["Link"])
+    link = body["Link"]
+    
+  
+    try:
+        yt = YouTube(link)
+        yt.streams.filter(adaptive=True, file_extension='mp4').get_by_itag(137).download()
+    except:
+        return {"error":"Connection Error"}  # to handle exception
+
+
+  
